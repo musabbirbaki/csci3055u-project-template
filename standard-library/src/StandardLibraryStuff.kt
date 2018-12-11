bf564a7f57f60128999c0a0cf1c8c9f9e0a562e0
musabbir.a.b@hotmail.com <musabbir.a.b@hotmail.com>
2018-12-11 10:25:21 -0500

    Adding example of Standard Library.

---
+++ standard-library/src/StandardLibraryStuff.kt
@@ -0,0 +1,47 @@
+import kotlin.reflect.jvm.internal.impl.protobuf.LazyStringArrayList
+
+/**Standard library**/
+
+fun main(args: Array<String>){
+    println("Hello, world")
+
+
+    val numbers: IntArray = intArrayOf(1, 2, 3, 4, 5)
+
+    val doubles: DoubleArray = doubleArrayOf(1.1, 2.2, 3.3, 4.4, 5.5)
+
+    val num: Int by lazy {
+        503
+    }
+
+    val nums: MutableList<Int> = mutableListOf(1,2,3) //mutable list
+    val readOnlyView: List<Int> = nums //immutable List
+    println(nums)
+
+    //append
+    nums.add(4)
+    //nums.add("s") can't do this since its type locked
+
+    nums.clear()
+
+    //first, last, filter
+    val items = listOf(1,2,3,4,5,6,7,7)
+    println(items.first())
+    println(items.last())
+    println("Odd Nums: " + items.filter { it % 2 == 1 })
+
+    //hashmap
+    val readWriteMap = hashMapOf("foo" to 1, "bar" to 2)
+    println(readWriteMap["foo"]) //get by value function
+    println(readWriteMap.get("bar"))
+
+    val strs = hashSetOf("a", "b", "c")
+    println("My set " + strs)
+
+    //Ranges
+    for (j in 1..4)
+        println("--" + j) //prints from inclusive 1 to inclusive 4
+    //println(j) this will give error
+
+    
+}
